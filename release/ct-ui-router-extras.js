0be29d0c64a4585da23c325edca488803cfe84ea
aessare <104900634+aessare@users.noreply.github.com>
2022-08-26 12:53:28 +0100

    check for undefined $$equals

--- release/ct-ui-router-extras.js
+++ release/ct-ui-router-extras.js
@@ -470,7 +470,7 @@ function $StickyStateProvider($stateProvider, uirextras_coreProvider) {
       }
 
       function paramsEqualForState(ownParams, stateParams, stateParams2) {
-        if (typeof ownParams.$$equals === 'function')
+        if (ownParams && typeof ownParams.$$equals === 'function')
           return ownParams.$$equals(stateParams, stateParams2);
         return equalForKeys(stateParams, stateParams2, ownParams);
       }
